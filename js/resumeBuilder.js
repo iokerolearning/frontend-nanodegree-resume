509004033b45456347972b713d34f9e6b1a087c5
Wenceslao Paez Chavez <wcpaez@gmail.com>
2017-09-18 15:37:03 -0500

    Add bio info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,84 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+  "name" : "Wenceslao Paez",
+  "role" : "Software Developer",
+  "contacts" : {
+    "mobile": "983437401",
+    "email": "wcpaez@gmail.com",
+    "github": "https://github.com/wcpaez",
+    "twitter": "",
+    "location": "Huancayo",
+  },
+  "welcomeMessage": "Hola, soy un super desarrollador",
+  "skills": ["JS", "RoR", "Testing"],
+  "biopic": ""
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "Colegio Particular Andino",
+      "location": "Huancayo",
+      "degree": "Primaria y secundaria",
+      "majors": [],
+      "dates": "1996 - 2016"
+    },
+    {
+      "name": "Colegio Particular Andino",
+      "location": "Huancayo",
+      "degree": "Primaria y secundaria",
+      "majors": [],
+      "dates": "1996 - 2016"
+    }
+  ],
+  "onlineCourses": [
+  ]
+}
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Osiptel",
+      "title": "Practicante profesional",
+      "location": 'Lima',
+      "dates": "2012-2013",
+      "description": 'Practicate profesional en el area de telecomunicaciones'  
+    },
+    {
+      "employer": "iokero",
+      "title": "Software Developer",
+      "location": 'Lima y Huancayo',
+      "dates": "2013-presente",
+      "description": 'Software developer con ROR'  
+    },
+    {
+      "employer": "Waggl INC",
+      "title": "Software developer",
+      "location": 'Lima, SF',
+      "dates": "2015-2017",
+      "description": 'Software developer with ROR'  
+    },
+    {
+      "employer": "Campodata INC",
+      "title": "Software developer",
+      "location": 'Lima, Puerto Rico',
+      "dates": "2017-presente",
+      "description": 'Software developer with ROR'  
+    },
+    {
+      "employer": "Instituto San Pedro",
+      "title": "Docente",
+      "location": 'Huancayo',
+      "dates": "2017-presente",
+      "description": 'Docente de Tecnologias WEB'  
+    }
+  ]
+}
+
+var projects = {
+
+}
+
+console.log(work);
